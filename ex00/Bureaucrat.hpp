12fa2ab22dbacccdcd276eb29e29d71db3d095ce
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-04 16:58:11 +0100

    Added definition of GradeTooHighException

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -7,11 +7,21 @@
 class Bureaucrat
 {
 private:
-	/* data */
+	const std::string _name;
+	unsigned int _grade;
 public:
 	Bureaucrat(void);
 	Bureaucrat(const Bureaucrat &toCopy);
 	Bureaucrat &operator=(const Bureaucrat &toCopy);
 	~Bureaucrat(void);
+
+	class GradeTooHighException : std::exception
+	{
+		public:
+			virtual const char* what() const throw()
+			{
+				return ("Grade is too high, the maximum is 1");
+			}
+	};
 };
 #endif
